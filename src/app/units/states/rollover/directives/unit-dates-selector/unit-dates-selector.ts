8cf00697c7ea076ec419277585147c8c837e6c9c
marccoVee <jason.vellucci@live.com>
2025-04-12 14:12:34 +1000

    feat: define skeleton TS class
    - define skeleton code for TS class for the `unit-dates-selector.ts` file

---
+++ src/app/units/states/rollover/directives/unit-dates-selector/unit-dates-selector.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'unit-dates-selector',
+  templateUrl: 'units/states/rollover/directives/unit-dates-selector/unit-dates-selector.tpl.html',
+})
+export class UnitDatesSelector {
+  constructor() {
+
+  }
+}
