9f673da08d7757243cf3e5d4a1e396b8fbf706a3
marccoVee <jason.vellucci@live.com>
2025-05-17 14:35:39 +1000

    chore: migrate `tutorials.coffee` - define `tutorials.ts` file

--- src/app/projects/states/tutorials/tutorials.ts
+++ src/app/projects/states/tutorials/tutorials.ts
@@ -1,12 +1,20 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'tutorials',
-  templateUrl: './tutorials.component.tpl.html',
-  styleUrls: ['./tutorials.component.scss'],
+  templateUrl: './tutorials.component.tpl.html', // External template file
+  styleUrls: ['./tutorials.component.scss'], // Optional: Add styles here
 })
 export class TutorialsComponent implements OnInit {
-  constructor() {}
+  @Input() unit: any; // Input property for unit data
+  sortOrder: string = ''; // Default sort order
 
-  ngOnInit() {}
+  ngOnInit(): void {
+    // Set the sort order based on the unit's tutorialStreamsCache size
+    if (this.unit?.tutorialStreamsCache?.size > 0) {
+      this.sortOrder = 'tutorialStream.name';
+    } else {
+      this.sortOrder = 'abbreviation';
+    }
+  }
 }
